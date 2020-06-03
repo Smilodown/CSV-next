7a74475b7a9a1bcd74534af7d61bbfe3069c3a19
Cycleurope <web@cycleurope.fr>
2020-06-03 23:53:03 +0200

    Update Navigation.js

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,34 +1,35 @@
 import Link from 'next/link'
 import { useState} from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
 
 const Navigation = () => {
 
     return (
         <header id="header">
-        <nav className="navbar navbar-expand-lg light-menu fixed-top">
-            <a href="#" className="navbar-brand">Cellier Saint Vincent</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                    FERMER
-                    <span className="bun"></span>
-                    <span className="bun"></span>
-                </button>
-            <div className="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
-                <ul className="navbar-nav">
-                    <li className="nav-item"><Link href="/" scroll={false}><a className="nav-link">Accueil</a></Link></li>
-                    <li className="nav-item"><Link href='/club' scroll={false}><a className="nav-link">Le Club</a></Link></li>
-                    <li className="nav-item"><Link href="/agenda" scroll={false}><a className="nav-link">Agenda</a></Link></li>
-                    <li className="nav-item"><Link href="/offers" scroll={false}><a className="nav-link">Offres</a></Link></li>
-                    <li className="nav-item"><Link href="/galleries" scroll={false}><a className="nav-link">Photos</a></Link></li>
-                    <li className="nav-item"><Link href="/info" scroll={false}><a className="nav-link">Infos</a></Link></li>
-                </ul>
-                <div className="text-center pb-5 d-block d-lg-none">
-                    <a href="" className="btn btn-dark btn-rounded btn-text-small-upper">Réserver</a>
-                </div>
+        <Navbar expand="lg" className="fixed-top">
+         <Navbar.Brand className="navbar-brand">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
+                FERMER
+                <span className="bun"></span>
+                <span className="bun"></span>
+            </Navbar.Toggle>
+            <Navbar.Collapse id="basic-navbar-nav">
+            <ul className="navbar-nav mr-auto ml-auto">
+                <li className="nav-item"><Link href="/" scroll={false}><a className="nav-link">Accueil</a></Link></li>
+                <li className="nav-item"><Link href='/club' scroll={false}><a className="nav-link">Le Club</a></Link></li>
+                <li className="nav-item"><Link href="/agenda" scroll={false}><a className="nav-link">Agenda</a></Link></li>
+                <li className="nav-item"><Link href="/offers" scroll={false}><a className="nav-link">Offres</a></Link></li>
+                <li className="nav-item"><Link href="/galleries" scroll={false}><a className="nav-link">Photos</a></Link></li>
+                <li className="nav-item"><Link href="/info" scroll={false}><a className="nav-link">Infos</a></Link></li>
+            </ul>
+            <div className="text-center pb-5 d-block d-lg-none">
+                <a href="" className="btn btn-dark btn-rounded btn-text-small-upper">Réserver</a>
             </div>
+            </Navbar.Collapse>
             <div className="d-none d-lg-block pr-4">
                 <Link href="/booking" scroll={false}><a className="btn btn-dark btn-rounded btn-text-small-upper">Réserver</a></Link>
             </div>
-        </nav>
+        </Navbar>
     </header>
     )
 }
